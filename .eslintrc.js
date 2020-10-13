0eb100a859c9fb115cce31377da1f6ff5ab3625e
Ruan Beukes <ruan.coder@gmail.com>
2020-10-13 10:51:04 +0800

    integrate ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,49 @@
 // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  // https://eslint.org/docs/user-guide/configuring#specifying-parser-options
+  // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/parser/README.md
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    // https://eslint.org/docs/user-guide/configuring#specifying-parser-options
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/parser/README.md
-    "parserOptions": { 
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12, // 12 = 2021
-        "sourceType": "module"
+    ecmaVersion: 12, // 12 = 2021
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    // disable the rule for all files
+    // vscode might pop-up a notification: There are multiple formatter for 'Javascript' files. I selected Prettier as default.
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  /*
+  // there is the option to apply to specific files only. 
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+      },
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
+  ],
+  */
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
     },
-    "settings": {
-        "react": {
-          "pragma": "React",
-          "version": "detect"
-        }
-    }
+  },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  // printWidth: 120, // I'll add this later, commented out for now to prevent too many changes.
+  arrowParens: 'avoid',
+};
